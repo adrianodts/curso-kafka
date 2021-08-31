e79f8edb4704b906413af1ca81dd5e5f0965657e
Adriano Dantas <adrianodts@msn.com>
2021-08-30 22:04:23 -0300

    Async Producer

--- go-kafka/cmd/producer/main.go
+++ go-kafka/cmd/producer/main.go
@@ -10,15 +10,8 @@ func main() {
 	deliveryChain := make(chan kafka.Event)
 	producer := NewKafkaProducer()
 	Publish("mensagem", "teste", producer, nil, deliveryChain)
-
-	// Sincrono
-	e := <-deliveryChain
-	msg := e.(*kafka.Message)
-	if msg.TopicPartition.Error != nil {
-		fmt.Println("Erro ao enviar mensagem")
-	} else {
-		fmt.Println("Mensagem enviada", msg.TopicPartition)
-	}
+	go AsyncDeliveryReport(deliveryChain)
+	//SyncDeliveryReport(deliveryChain)
 	producer.Flush(1000)
 }
 
@@ -46,33 +39,27 @@ func Publish(msg string, topic string, producer *kafka.Producer, key []byte, del
 		return err
 	}
 	return nil
-	/*
-	defer p.Close()
+}
 
-	// Delivery report handler for produced messages
-	go func() {
-		for e := range p.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
-				} else {
-					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
-				}
+func AsyncDeliveryReport(deliveryChain chan kafka.Event) {
+	for e := range deliveryChain {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Println("Erro ao enviar mensagem")
+			} else {
+				fmt.Println("Mensagem enviada", ev.TopicPartition)
 			}
 		}
-	}()
-
-	// Produce messages to topic (asynchronously)
-	topic := "teste"
-	for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
-		p.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-			Value:          []byte(word),
-		}, nil)
 	}
+}
 
-	// Wait for message deliveries before shutting down
-	p.Flush(15 * 1000)
-	*/
+func SyncDeliveryReport(deliveryChain chan kafka.Event) {
+	e := <-deliveryChain
+	msg := e.(*kafka.Message)
+	if msg.TopicPartition.Error != nil {
+		fmt.Println("Erro ao enviar mensagem")
+	} else {
+		fmt.Println("Mensagem enviada", msg.TopicPartition)
+	}
 }
